caa8961c2c713bdfaf660f8dd69c11c5f206e011
Deepaksharma-39 <d33paksharma.39@gmail.com>
2024-10-29 11:21:32 +0530

    some layout changes

--- src/sections/cabprovidor/companyManagement/companyOverview/CompanyRate1/CompanyRateListing.js
+++ src/sections/cabprovidor/companyManagement/companyOverview/CompanyRate1/CompanyRateListing.js
@@ -1,23 +1,19 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 // assets
-import {
-    Button,
-  CircularProgress,
-  Stack
-} from '@mui/material';
+import { Button, CircularProgress, Stack } from '@mui/material';
 import CompanyRateReactTable from './CompanyRateReactTable';
 import Header from 'components/tables/genericTable/Header';
 import WrapperButton from 'components/common/guards/WrapperButton';
 import { Add } from 'iconsax-react';
 import CompanyRate from './CompanyRate';
+import axiosServices from 'utils/axios';
 
 // ==============================|| REACT TABLE - EDITABLE CELL ||============================== //
 
-
 const token = localStorage.getItem('serviceToken');
 
-const CompanyRateListing = ({companyName,id}) => {
+const CompanyRateListing = ({ companyName, id }) => {
   const [data, setData] = useState([]);
   const [skipPageReset, setSkipPageReset] = useState(false);
   const [companyRate, setCompanyRate] = useState([]);
@@ -30,17 +26,10 @@ const CompanyRateListing = ({companyName,id}) => {
 
   useEffect(() => {
     const fetchdata = async () => {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/company/unwind/rates?companyId=${id}`,
-        {
-          headers: {
-            Authorization: `${token}`
-          }
-        }
-      );
+      const response = await axiosServices.get(`/company/unwind/rates?companyId=${id}`);
       setCompanyList(response.data.data);
       setLoading(false);
-     console.log("response.data",response.data.data);
+      console.log('response.data', response.data.data);
     };
 
     fetchdata();
@@ -61,7 +50,7 @@ const CompanyRateListing = ({companyName,id}) => {
         <Stack gap={1} spacing={1}>
           <Header OtherComp={({ loading }) => <ButtonComponent loading={loading} onAddRate={handleAddRate} />} />
 
-          {(companyList.length !== 0) && (
+          {companyList.length !== 0 && (
             <CompanyRateReactTable
               data={companyList}
               page={page}
@@ -75,7 +64,7 @@ const CompanyRateListing = ({companyName,id}) => {
           )}
         </Stack>
       ) : (
-        <CompanyRate id={id} companyName={companyName}/> // Render CompanyList1 when the state is true
+        <CompanyRate id={id} companyName={companyName} /> // Render CompanyList1 when the state is true
       )}
     </>
   );
@@ -84,19 +73,19 @@ const CompanyRateListing = ({companyName,id}) => {
 export default CompanyRateListing;
 
 const ButtonComponent = ({ loading, onAddRate }) => {
-    return (
-      <Stack direction="row" spacing={1} alignItems="center">
-        <WrapperButton>
-          <Button
-            variant="contained"
-            startIcon={loading ? <CircularProgress size={20} color="inherit" /> : <Add />}
-            onClick={onAddRate} // Call the onAddRate function when button is clicked
-            size="small"
-            disabled={loading} // Disable button while loading
-          >
-            {loading ? 'Loading...' : ' Add Rate'}
-          </Button>
-        </WrapperButton>
-      </Stack>
-    );
-  };
+  return (
+    <Stack direction="row" spacing={1} alignItems="center">
+      <WrapperButton>
+        <Button
+          variant="contained"
+          startIcon={loading ? <CircularProgress size={20} color="inherit" /> : <Add />}
+          onClick={onAddRate} // Call the onAddRate function when button is clicked
+          size="small"
+          disabled={loading} // Disable button while loading
+        >
+          {loading ? 'Loading...' : ' Add Rate'}
+        </Button>
+      </WrapperButton>
+    </Stack>
+  );
+};
